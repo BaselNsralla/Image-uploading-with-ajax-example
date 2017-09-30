3cc8e220fb30be60ff48caa611e440faad4978b9
Baseln <basel.obeid9661@gmail.com>
2017-09-30 12:59:04 +0200

    Update imageUpload.php

--- root/imageUpload.php
+++ root/imageUpload.php
@@ -4,7 +4,7 @@
      $target = "uploads/".basename($_FILES["file"]["name"]);
 
     //$_FILES["imageName"]["tmp_name"] is the path to the file that has been uploaded to 
-    //the temp directory with php by default when you submited, the file should be an image if 
+    //the temp directory in the server when you submited, the file should be an image if 
     //you are a good person, you can add security checks.
     move_uploaded_file($_FILES["file"]["tmp_name"],$target);
   
